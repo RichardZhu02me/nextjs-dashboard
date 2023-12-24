f8e93f7c8aa8c17457a55c8c4c2f972bae15127a
Richard <87499486+RichardZhu02me@users.noreply.github.com>
2023-12-24 11:07:18 -0500

    first commit
    following the tutorial on nextjs.org, we push the repository to github as part of setting up the database

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Caveat, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const caveat = Caveat({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'] , weight:"700"});;
